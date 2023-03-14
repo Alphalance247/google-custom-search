23dc431e22498e161138f395015d49d5822c756a
Alphalance247 <al.moshood017@gmail.com>
2023-03-14 18:39:21 +0100

    mobile-responsiveness added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const btn = document.querySelector(".btn");
 const inputValue = document.querySelector(".input");
 const parentEl = document.querySelector(".search-results");
+const API_URL = `https://www.googleapis.com/customsearch/v1?key=AIzaSyA_sUs7ZfeoLy31HX3iHUFaetPOJCbEiSY&cx=d7aff7dce5bda40e0&q
+`;
+
+///SETTING TIMEOUT FOR UNSTABLE INTERNET CONNECTION
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
 
 ///Refacturing the DOM elemnt
 const adjacentHTML = function (data) {
@@ -16,7 +27,7 @@ const adjacentHTML = function (data) {
                 <div class="paraf-1">
                   <img src="download (1).png" alt="" />
                     <div class="title_url">
-                      <h3>${el.link}</h3>
+                      <h3>${el.displayLink}</h3>
                       <p> ${el.formattedUrl}</p>
                     </div>
                  </div>
@@ -37,26 +48,37 @@ const adjacentHTML = function (data) {
 };
 
 ////Listening for the event and rendering search results
-
 btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  const val = inputValue.value;
+  try {
+    e.preventDefault();
+    const val = inputValue.value;
 
-  ///fetching request using async await function
-  const getRequest = async function (query) {
-    const res = await fetch(
-      `https://www.googleapis.com/customsearch/v1?key=AIzaSyA_sUs7ZfeoLy31HX3iHUFaetPOJCbEiSY&cx=d7aff7dce5bda40e0&q=${query}
-      `
-    );
+    if (!val) throw new error("🌟🌟 data is not available");
 
-    const data = await res.json();
+    ///FETCHING REQUEST USING ASYNC AWAIT
+    const getRequest = async function (query) {
+      const res = await Promise.race([
+        fetch(`${API_URL}=${query}`),
+        timeout(10),
+      ]);
 
-    console.log(data);
+      const data = await res.json();
 
-    adjacentHTML(data);
-  };
+      console.log(data);
 
-  //clearing the parent elemebnt
-  parentEl.innerHTML = "";
-  getRequest(val);
+      if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+      if (!data || (Array.isArray(data) && data.length === 0))
+        return console.log(`${data.error.message}`);
+      ("data not available 🌟🌟");
+
+      adjacentHTML(data);
+    };
+
+    //clearing the parent elemebnt
+    parentEl.innerHTML = "";
+    getRequest(val);
+  } catch (err) {
+    console.error(err.message, "🌟🌟");
+  }
 });
