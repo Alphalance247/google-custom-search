dedafeb43ddd1444381bf250aedcf5e83aa8ea81
Alphalance247 <al.moshood017@gmail.com>
2023-03-15 12:36:25 +0100

    intersection obeserver api features added

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btn = document.querySelector(".btn");
-const observer = document.querySelector(".stop");
-const View = document.querySelector(".search-enter");
+
 const inputValue = document.querySelector(".input");
 const parentEl = document.querySelector(".search-results");
 const API_URL = `https://www.googleapis.com/customsearch/v1?key=AIzaSyA_sUs7ZfeoLy31HX3iHUFaetPOJCbEiSY&cx=d7aff7dce5bda40e0&q
@@ -86,14 +85,35 @@ btn.addEventListener("click", function (e) {
 });
 
 //////Adding the intersection observer api
-const initialcords = observer.getBoundingClientRect();
+// const initialcords = observer.getBoundingClientRect();
 
-// console.log(initialcords);
+// // console.log(initialcords);
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY > initialcords.top) {
-    View.classList.add("sticky");
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY > initialcords.top) {
+//     View.classList.add("sticky");
+//   } else {
+//     View.classList.remove("sticky");
+//   }
+// });
+
+const header = document.querySelector(".stop");
+const nav = document.querySelector(".search-enter");
+
+const callback = function (entries) {
+  [entries] = entries;
+  // console.log(...entries);
+  if (!entries.isIntersecting) {
+    nav.classList.add("sticky");
   } else {
-    View.classList.remove("sticky");
+    nav.classList.remove("sticky");
   }
+  // console.log(entries);
+};
+
+const observes = new IntersectionObserver(callback, {
+  root: null,
+  threshold: 0,
 });
+
+observes.observe(header);
